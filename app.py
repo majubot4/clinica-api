922a3ffb93c618bb09055e0e2c537a0ca3f3aae8
Maria Júlia Campos Silveira Chaves <mariajuliacamposschaves@gmail.com>
2026-07-13 23:14:52 -0300

    ultimo commit

--- app.py
+++ app.py
@@ -1,31 +1,21 @@
 from flask import Flask
 from banco import banco
 
-
 app = Flask(__name__)
 
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 
 banco.init_app(app)
 
-
+import modelos  
 from rotas import *
-
 
 with app.app_context():
     banco.create_all()
-
 
 @app.route("/")
 def inicio():
     return {
         "mensagem": "API Clínica Médica funcionando!"
     }
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ rotas/__init__.py
@@ -0,0 +1,4 @@
+from rotas.paciente_rotas import *
+from rotas.usuario_rotas import *
+from rotas.medicos_rotas import *
+from rotas.consultas_rotas import *
