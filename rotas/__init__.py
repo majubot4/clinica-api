09e624477901713020975e644c1589d2634f5db1
Maria Júlia Campos Silveira Chaves <mariajuliacamposschaves@gmail.com>
2026-07-13 23:31:42 -0300

    ultimo commit

--- rotas/__init__.py
+++ rotas/__init__.py
@@ -1,4 +1,4 @@
 from rotas.paciente_rotas import *
 from rotas.usuario_rotas import *
-from rotas.medicos_rotas import *
-from rotas.consultas_rotas import *
+from rotas.medico_rotas import *
+from rotas.consulta_rotas import *
